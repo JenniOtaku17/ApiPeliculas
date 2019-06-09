94bb06e5c4111c2049d5ce59d4ab20bc5da82b86
Jennifer <20175329@itla.edu.do>
2019-06-09 09:40:05 -0800

    Servicios

---
+++ ApiPelicula/Service/PeliculaService.cs
@@ -0,0 +1,24 @@
+﻿using ApiPelicula.Model;
+using ApiPelicula.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPelicula.Service
+{
+    public class PeliculaService
+    {
+        public readonly PeliculaDbContext _PeliculaDB;
+        public PeliculaService(PeliculaDbContext PeliculaDB)
+        {
+            _PeliculaDB = PeliculaDB;
+        }
+
+        public List<Pelicula> VerListado()
+        {
+            var PeliBuscada = _PeliculaDB.Pelicula.OrderByDescending(x => x.PeliculaId).ToList();
+            return PeliBuscada;
+        }
+    }
+}
