00b350bb2485fa377df655c5bfbe39c8ededfd70
Jennifer <20175329@itla.edu.do>
2019-06-09 08:56:28 -0800

    DbContext

---
+++ ApiPelicula/Persistance/PeliculaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ApiPelicula.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPelicula.Persistance
+{
+    public class PeliculaDbContext: DbContext
+    {
+        public PeliculaDbContext(DbContextOptions opciones): base(opciones)
+        {
+
+        }
+
+        public virtual DbSet<Pelicula> Pelicula { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelB)
+        {
+            new Pelicula.Map(modelB.Entity<Pelicula>());
+            base.OnModelCreating(modelB);
+        }
+    }
+}
