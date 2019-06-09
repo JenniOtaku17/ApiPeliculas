c820e509a8808fdd8e15f2fe4bacd01895537c53
Jennifer <20175329@itla.edu.do>
2019-06-09 07:35:17 -0800

    Agregando el modelo

---
+++ ApiPelicula/Model/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPelicula.Model
+{
+    public class Pelicula
+    {
+        public int PeliculaId { get; set; }
+        public string titulo { get; set; }
+        public string categoria { get; set; }
+        public int anio { get; set; }
+    }
+}
